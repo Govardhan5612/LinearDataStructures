190274137a173ecc3c779f4f07d64588287511e3
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-12-11 18:36:25 +0530

    Create a simple linked list

---
+++ com/bridgelabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.linkedlist;
+
+import java.util.LinkedList;
+
+/**
+ * @author Govardhan Reddy
+ */
+public class LinkedListMain {
+    /**
+     * create a linked list
+     */
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        System.out.println(list);
+    }
+}
